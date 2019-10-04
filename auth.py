8c49b6f36281f434d8c83a96d4b014c3ced7adf9
Renata O. Barbosa <33003695+RenataOliveira1106@users.noreply.github.com>
2019-10-04 01:51:22 -0300

    Add files via upload

---
+++ auth.py
@@ -0,0 +1,4 @@
+consumer_key        = 'iuIgER0xzGkYEdiYOixxgViNT'
+consumer_secret     = 'pYTJOwye35RbgoMYTPE3S7Xu33FAuKCv87M3HMnMg7scMun8BQ'
+access_token        = '1179966243371180034-zhFsFidccX292ne6wLezPZG59JT0zr '
+access_token_secret = 'gw0VDSfB9G5OwERbI8c35UM24oVyls31wWIu5YlB1K3zS'
